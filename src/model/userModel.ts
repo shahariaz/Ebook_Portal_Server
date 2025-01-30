543dd91b295ee8da96966c636e860f9b98fb190f
shahariaz <shahariaz.info@gmail.com>
2025-01-30 20:53:27 +0600

    feat: Write User Model

---
+++ src/model/userModel.ts
@@ -0,0 +1,47 @@
+import { timeStamp } from "console";
+import mongoose from "mongoose";
+import { IUser } from "../types/interface";
+
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    fullName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    avatar: {
+      type: String,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    lastLogin: {
+      type: Date,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,4 +1,28 @@
+import { Document } from "mongoose";
+
 export interface IDatabaseConnection {
   retryCount: number;
   isConnected: boolean;
+}
+
+export enum Role {
+  ADMIN = "admin",
+  USER = "user",
+  SUPERADMIN = "superadmin",
+  AUTHOR = "author",
+}
+export interface IUser extends Document {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+  role: "user" | "admin" | "moderator";
+  isVerified: boolean;
+  avatar?: string;
+  lastLogin?: Date;
+  createdAt: Date;
+  updatedAt: Date;
+  fullName: string; // Virtual field
+  comparePassword(candidatePassword: string): Promise<boolean>;
 }
